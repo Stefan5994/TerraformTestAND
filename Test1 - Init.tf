556ddb28572bcc16d4b72ce89649abf0ab83e3c6
Stefan Buse <stefan.buse10@gmail.com>
2021-05-06 16:59:02 +0100

    Added ALB, comments to define where resources are, Target Groups and associations. Next are launch template and ASG, then another branch or Terraform file group will demonstrate usage of modules and variables altogether

--- Test1 - Init.tf
+++ Test1 - Init.tf
@@ -2,12 +2,16 @@ provider "aws" {
     region = "us-east-1" 
 }
 
+# VPC defined below
+
 resource "aws_vpc" "test_vpc" {
     cidr_block = "10.5.0.0/16"
     tags = {
         Name = "TestVPC_TF"
     }
 }
+
+# Subnets public and private defined below
 
 resource "aws_subnet" "priv1" {
     vpc_id = aws_vpc.test_vpc.id
@@ -42,6 +46,7 @@ resource "aws_subnet" "pub2" {
     }
 }
 
+# Internet Gateway defined below
 resource "aws_internet_gateway" "PubIGW"{
     vpc_id = aws_vpc.test_vpc.id
     tags = {
@@ -49,6 +54,8 @@ resource "aws_internet_gateway" "PubIGW"{
 
     }   
 }
+
+# Route Tables and route associations defined below
 
 resource "aws_route_table" "public_route" {
     vpc_id = aws_vpc.test_vpc.id
@@ -88,3 +95,78 @@ resource "aws_route_table_association" "PrivRoutAssoc2" {
     route_table_id = aws_route_table.private_route.id
     subnet_id = aws_subnet.priv2.id
 }
+
+# Launch template defined below
+
+#resource "aws_launch_template" "LaunchTemplate_TF" {
+ #   name = "LT_TF"
+  #  description = "Launch template for instances to be launched with simple text and instance ID retrieval"
+   # image-id =     
+    #}
+#}
+
+# Autoscaling group defined below 
+
+# resource "aws_autoscaling_group" "ASGTest" {
+ #   min_size = 0
+  #  max_size = 0
+   # vpc_zone_identifier = [aws_subnet.priv1.id,aws_subnet.priv2.id]
+#}
+
+# Application Load Balancer defined below including corresponding Security Group
+
+resource "aws_security_group" "ALB_SG" {
+    description = "Allow HTTP and HTTPS traffic from any internet address"
+    vpc_id = aws_vpc.test_vpc.id
+    ingress {
+        description = "HTTPS from the internet"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"    
+        cidr_blocks = ["0.0.0.0/0"]
+    }   
+    ingress {
+        description = "HTTPS from the internet"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"    
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        description = "All traffic to the internet"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        
+    }
+    
+    tags = {
+        Name = "ALB_SG_TF"
+    }
+}
+resource "aws_lb_target_group" "ALB_TG_TF" {
+    name = "TargetGroup-TF"
+    vpc_id = aws_vpc.test_vpc.id
+    port = 80
+    protocol = "HTTP"
+    
+}
+resource "aws_lb_listener" "ALB_Listener_TF" {
+    load_balancer_arn = aws_lb.alb-TF.arn
+    port = 80
+    protocol = "HTTP"
+    default_action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.ALB_TG_TF.id
+    }
+}
+resource "aws_lb" "alb-TF" {
+    name = "ALB-TF"
+    internal = false
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.ALB_SG.id]
+    subnets = [aws_subnet.pub1.id,aws_subnet.pub2.id]
+
+}
+
