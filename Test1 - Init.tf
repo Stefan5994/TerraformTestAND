3b69e409f4b1326e8d6bef405b0ded4e3062a129
Stefan Buse <stefan.buse10@gmail.com>
2021-05-06 02:00:10 +0100

    Added subnets currently only tagged as private and public

--- Test1 - Init.tf
+++ Test1 - Init.tf
@@ -8,3 +8,36 @@ resource "aws_vpc" "test_vpc" {
         Name = "TerraformTestVPC"
     }
 }
+
+resource "aws_subnet" "priv1" {
+    vpc_id = aws_vpc.test_vpc.id
+    cidr_block = "10.5.1.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "PrivateSubnet1"
+    }
+}
+resource "aws_subnet" "priv2" {
+    vpc_id = aws_vpc.test_vpc.id
+    cidr_block = "10.5.2.0/24"
+    availability_zone = "us-east-1b"
+    tags = {
+        Name = "PrivateSubnet2"
+    }
+}
+resource "aws_subnet" "pub1" {
+    vpc_id = aws_vpc.test_vpc.id
+    cidr_block = "10.5.3.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "PublicSubnet1"
+    }
+}
+resource "aws_subnet" "pub2" {
+    vpc_id = aws_vpc.test_vpc.id
+    cidr_block = "10.5.4.0/24"
+    availability_zone = "us-east-1b"
+    tags = {
+        Name = "PublicSubnet2"
+    }
+}
