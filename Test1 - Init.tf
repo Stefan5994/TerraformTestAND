a5dd3ff975a106c7a9d65a3769115db9de663463
Stefan Buse <stefan.buse10@gmail.com>
2021-05-06 02:52:16 +0100

    Added route tables and associated with relevant subnets including IGW. Current configuration is suboptimal, being fully declared in the main.tf file. Following commits will demonstrate use of modules and variables as well

--- Test1 - Init.tf
+++ Test1 - Init.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_vpc" "test_vpc" {
     cidr_block = "10.5.0.0/16"
     tags = {
-        Name = "TerraformTestVPC"
+        Name = "TestVPC_TF"
     }
 }
 
@@ -14,7 +14,7 @@ resource "aws_subnet" "priv1" {
     cidr_block = "10.5.1.0/24"
     availability_zone = "us-east-1a"
     tags = {
-        Name = "PrivateSubnet1"
+        Name = "PrivateSubnet1_TF"
     }
 }
 resource "aws_subnet" "priv2" {
@@ -22,7 +22,7 @@ resource "aws_subnet" "priv2" {
     cidr_block = "10.5.2.0/24"
     availability_zone = "us-east-1b"
     tags = {
-        Name = "PrivateSubnet2"
+        Name = "PrivateSubnet2_TF"
     }
 }
 resource "aws_subnet" "pub1" {
@@ -30,7 +30,7 @@ resource "aws_subnet" "pub1" {
     cidr_block = "10.5.3.0/24"
     availability_zone = "us-east-1a"
     tags = {
-        Name = "PublicSubnet1"
+        Name = "PublicSubnet1_TF"
     }
 }
 resource "aws_subnet" "pub2" {
@@ -38,6 +38,53 @@ resource "aws_subnet" "pub2" {
     cidr_block = "10.5.4.0/24"
     availability_zone = "us-east-1b"
     tags = {
-        Name = "PublicSubnet2"
+        Name = "PublicSubnet2_TF"
     }
+}
+
+resource "aws_internet_gateway" "PubIGW"{
+    vpc_id = aws_vpc.test_vpc.id
+    tags = {
+        Name = "IGW_TF"
+
+    }   
+}
+
+resource "aws_route_table" "public_route" {
+    vpc_id = aws_vpc.test_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.PubIGW.id
+    }
+
+    tags = {
+        Name = "PublicRouteTable_TF"
+    }
+}
+
+resource "aws_route_table_association" "PubRoutAssoc1" {
+    route_table_id = aws_route_table.public_route.id
+    subnet_id = aws_subnet.pub1.id
+}
+
+resource "aws_route_table_association" "PubRoutAssoc2" {
+    route_table_id = aws_route_table.public_route.id
+    subnet_id = aws_subnet.pub2.id
+}
+
+resource "aws_route_table" "private_route" {
+    vpc_id = aws_vpc.test_vpc.id
+    tags = {
+        Name = "PrivateRouteTable_TF"
+    }
+}
+
+resource "aws_route_table_association" "PrivRoutAssoc1" {
+    route_table_id = aws_route_table.private_route.id
+    subnet_id = aws_subnet.priv1.id
+}
+
+resource "aws_route_table_association" "PrivRoutAssoc2" {
+    route_table_id = aws_route_table.private_route.id
+    subnet_id = aws_subnet.priv2.id
 }
